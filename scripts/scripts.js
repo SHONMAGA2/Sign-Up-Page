ba0274d4e072cffb5d53cecff83d6faae09eccfb
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-12-17 14:25:02 +0200

    saved pages

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+     const formData = new FormData(form);
+
+     const name = formData.get('name');
+     const email = formData.get('email');
+     const password = formData.get('password');
+     console.log('Name:', name);
+     console.log('Email:', email);
+     console.log('Password:', password);
+
+});
